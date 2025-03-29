5c1a37079c214450a7e647ee89a0f7c0e7f9c101
ipsitabhattacharya <bhattacharyaipsita573@gmail.com>
2025-03-29 21:37:05 +0530

    Add files via upload

---
+++ assign 2.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+
+	
+	printf("ADAMAS UNIVERSITY");
+	return 0;
+}
---
+++ programm for cse 1.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	int b=30;
+	int c=a+b;
+	
+	printf("%d",c);
+	return 0;
+}
