d32119ad9e07f1e651130f5b74a6171ada428250
ipsitabhattacharya <bhattacharyaipsita573@gmail.com>
2025-03-29 21:44:27 +0530

    Add files via upload

---
+++ prog 4.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	scanf("%d",&a);
+	printf("%d*1=%d",a,a*1);
+	printf("%d*2=%d",a,a*2);
+	printf("%d*3=%d",a,a*3);
+	printf("%d*4=%d",a,a*4);
+	printf("%d*5=%d",a,a*5);
+	printf("%d*6=%d",a,a*6);
+	printf("%d*7=%d",a,a*7);
+	printf("%d*8=%d",a,a*8);
+	printf("%d*9=%d",a,a*9);
+	printf("%d*10=%d",a,a*10);
+	printf("%d*1=%d",a,a*1);
+	return 0;
+}
