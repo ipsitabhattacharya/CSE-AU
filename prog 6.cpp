0e82fe6d6b781030fabe874b87eb3b5cac0196bc
ipsitabhattacharya <bhattacharyaipsita573@gmail.com>
2025-03-29 23:32:59 +0530

    Add files via upload

---
+++ prog 6.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	float si,p=200,r=5,t=2;
+	si=(p*r*t)/100;
+	printf("%f",si);
+	return 0;
+}
---
+++ prog 7.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	float area;
+	int r=10;
+	area=3.142*r*r;
+	printf("%f",area);
+	return 0;
+}
